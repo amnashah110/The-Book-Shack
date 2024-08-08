b32471d047721044fedd22d3e426b357448561cb
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-08-09 01:21:29 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    `mongodb+srv://amnashah834:acer2700@book-store-mern.8xx4bel.mongodb.net/booksCollection?retryWrites=true&w=majority`;
+   
